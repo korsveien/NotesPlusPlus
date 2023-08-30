ea5021cef4dcfb007b326e2fc0d3b51334e72525
peder <peder@korsveien.me>
2023-08-30 15:54:21 +0200

    Initial Commit

---
+++ Notes++/Notes__App.swift
@@ -0,0 +1,17 @@
+//
+//  Notes__App.swift
+//  Notes++
+//
+//  Created by Peder on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Notes__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
